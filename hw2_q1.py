f3c2aee7fd3469ee87e43cd8b1fd460acb83f1bf
michal magid <michal.magid@live.biu.ac.il>
2026-05-14 17:09:00 +0300

    addind q1 solution

--- hw2_q1.py
+++ hw2_q1.py
@@ -17,23 +17,24 @@ MORSE_CODE = {'A': '.-',     'B': '-...',   'C': '-.-.',
               "'": '.----.', '-': '-....-',
               }
 
+def word_to_morse(word: str) -> str:
+    return "".join(MORSE_CODE.get(c, "") for c in word)
+
+def line_to_morse(line: str) -> str:
+    return "\n".join(word_to_morse(word) for word in line.split())
+
 
 def english_to_morse(
     input_file: str = "lorem.txt",
     output_file: str = "lorem_morse.txt"
 ):
-    """Convert an input text file to an output Morse code file.
+    with open(input_file) as file:
+        text = file.read().upper()
 
-    Notes
-    -----
-    This function assumes the existence of a MORSE_CODE dictionary, containing a
-    mapping between English letters and their corresponding Morse code.
+    result = "\n".join(line_to_morse(line) for line in text.splitlines())
 
-    Parameters
-    ----------
-    input_file : str
-        Path to file containing the text file to convert.
-    output_file : str
-        Name of output file containing the translated Morse code. Please don't change
-        it since it's also hard-coded in the tests file.
-    """
+    with open(output_file, "w") as file:
+        file.write(result)
+
+if __name__ == "__main__":
+    english_to_morse()
